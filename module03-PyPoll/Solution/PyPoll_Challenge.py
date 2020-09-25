cc537412caabb2bbb6df16bc4c9dc49b250a9460
Jonasz Karwacki <jonaszakr@gmail.com>
2020-09-24 18:09:20 -0700

    Display counties to consolue

--- module03-PyPoll/Solution/PyPoll_Challenge.py
+++ module03-PyPoll/Solution/PyPoll_Challenge.py
@@ -99,14 +99,14 @@ with open(file_to_save, "w") as txt_file:
         # 6c: Calculate the percent of total votes for the county.
         vote_percentage = round(county_vote_count / total_votes * 100, 1)
 
-         # 6d: Print the county results to the terminal.
-         print(f"{county}: {vote_percentage}% ({county_vote_count}))
+        # 6d: Print the county results to the terminal.
+        print(f"{county}: {vote_percentage}% ({county_vote_count})")
 
-         # 6e: Save the county votes to a text file.
+        # 6e: Save the county votes to a text file.
 
-         # 6f: Write a decision statement to determine the winning county and get its vote count.
+        # 6f: Write a decision statement to determine the winning county and get its vote count.
 
-
+    print()
     # 7: Print the county with the largest turnout to the terminal.
 
 
